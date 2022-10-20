2bba501db6aa0f83cd61cee21fc5a61e10fead6b
Kamil-Wojciechowski <wojciechowski.kamil1@outlook.com>
2022-10-20 20:55:19 +0200

    CU-2ya0gvb - WEB-001: Inicjalizacja Repo + Sp

---
+++ src/main/java/com/uz/shop/olxd/OlxdApplication.java
@@ -0,0 +1,13 @@
+package com.uz.shop.olxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlxdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlxdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uz/shop/olxd/OlxdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uz.shop.olxd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlxdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
